b5877c6aa987ab15f7ac32bcd7654271892cb7a0
Christina Chronister <cchronister743@gmail.com>
2018-04-13 21:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+const logDriverNames = function (drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+    console.log(driver.name);
+    }
+  });
+};
+
+const driversByRevenue = function (drivers, revenue) {
+  return drivers.sort(revenue);
+}
