3a4f96daf6123f921cc1cba684ab6417cdf58592
Christina Chronister <cchronister743@gmail.com>
2018-04-13 22:17:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ const driversByName = function (drivers) {
 const totalRevenue = function (drivers) {
   return drivers.reduce(function (total, currentDriver) {
     return currentDriver.revenue + total;
-  })
-}
+  }, 0);
+};
 
